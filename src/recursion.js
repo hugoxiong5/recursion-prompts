2e72da38db470a9b8180781e7c7bd42fdce650b3
Hugo Xiong <hugoxiong@gmail.com>
2020-05-02 23:06:25 -0700

    added divide function

--- src/recursion.js
+++ src/recursion.js
@@ -186,7 +186,7 @@ var multiply = function(x, y) {
   if ( y === 0) {
     return 0;
   }
-  
+
   if (x < 0 && y < 0) {
     return -x + multiply(-x, -y - 1);
   }
@@ -203,9 +203,50 @@ var multiply = function(x, y) {
 
 };
 
+6-3-3
+
+6, 3
+
 // 13. Write a function that divides two numbers without using the / operator or
 // Math methods to arrive at an approximate quotient (ignore decimal endings).
 var divide = function(x, y) {
+  let quotient = 0;
+  if (y === 0) {
+    return NaN;
+  }
+
+  if (x < 0 && y < 0) {
+    if (-x < -y) {
+      return 0;
+    }
+    quotient += 1 + divide(-x - -y, -y);
+    return quotient;
+  }
+
+  if (x < 0 && y > 0) {
+    if (-x < y) {
+      return 0;
+    }
+    quotient += 1 + divide(-x - y, y);
+    return quotient;
+  }
+
+  if (x > 0 && y < 0) {
+    if (x < -y) {
+      return 0;
+    }
+    quotient += 1 + divide(x - -y, -y);
+    return quotient;
+  }
+
+  if (x >= 0 && y > 0) {
+    if (x < y) {
+      return 0;
+    }
+    quotient += 1 + divide(x - y, y);
+    return quotient;
+  }
+
 };
 
 // 14. Find the greatest common divisor (gcd) of two positive numbers. The GCD of two
